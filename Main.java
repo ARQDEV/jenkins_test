f39df773654600fc95552241577e1366845249df
skatalites <theskatalites@gmail.com>
2020-07-18 21:21:19 -0500

    New changes

--- Main.java
+++ Main.java
@@ -4,6 +4,5 @@ public class Main {
 		System.out.println("Hola Mundo desde Java");
 		System.out.println("Nuevos Cambios 2!!");
 		System.out.println("Nuevos Cambios 3!!");
-		System.out.println("Nuevos Cambios 4!!");
 	}
 }
